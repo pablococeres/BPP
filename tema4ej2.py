f4d1370162cbb9a2025166845065c6eeb327b0cd
pablococeres <pablosocialmedia@gmail.com>
2021-06-24 13:01:14 +0200

    Tema4 ej2

---
+++ tema4ej2.py
@@ -0,0 +1,12 @@
+mi_lista = [3, 4, 8, 5, 5, 22, 13]
+
+def es_primo(n):
+    primo = True
+    for i in range(2, n):
+        if(n%i == 0):
+            primo = False
+    return primo
+
+primos = list(filter(es_primo, mi_lista))
+
+print(primos)
