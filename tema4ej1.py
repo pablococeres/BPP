4f5149f99d3e15496ff4c23e92f7e9c235eb5773
pablococeres <pablosocialmedia@gmail.com>
2021-06-24 13:00:51 +0200

    Tema 4 ej1

---
+++ tema4ej1.py
@@ -0,0 +1,8 @@
+# import pdb
+# pdb.set_trace()
+
+mi_lista = [[2, 4, 1], [1,2,3,4,5,6,7,8], [100,250,43]]
+[print(max(a)) for a in mi_lista]
+listita = [1,1,2,3]
+a=0
+[a if a>1 else a*2 for item in listita]
